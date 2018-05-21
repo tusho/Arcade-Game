9fcd1e479fbd6d884461289c480e44e189d832ca
Armin Kuhestani <armin.kuhestani@gmail.com>
2018-05-21 13:31:19 +1000

    added player classes and render function

--- js/app.js
+++ js/app.js
@@ -8,3 +8,24 @@ var Enemy = function() {
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
+
+Enemy.prototype.update = function(dt) {
+  this.x = (this.x + this.speed);
+  this.y = 83*this.row;
+};
+
+var allEnemies = [];
+allEnemies.push(Enemy);
+
+var Player = function() {
+    this.sprite = 'images/char-boy.png';
+    this.x = 5;
+    this.y = 5;
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+var player = new Player();
+var enemy = new Enemy();
